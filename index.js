09797d42c601a367101f7d2ddd51156619358c5c
Rachel Teano <rp.teano@ufl.edu>
2025-05-24 22:27:56 -0400

    modified index.js and corrected the if...else statements

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function greet() {
+    var name = prompt('What is your name?');
+    alert('Hello, ' + name);
+
+    var ageInput = prompt('How old are you?');
+    var age = parseInt(ageInput);
+
+    var passedBirthday = confirm('Has your birthday passed this year?');
+    const currentYear = new Date().getFullYear();
+    let birthYear = currentYear - age;
+    let beforeBirthday = currentYear - age - 1;
+
+    if (passedBirthday == true) {
+        alert('You were born in ' + birthYear);
+    } else {
+        alert('You were born in ' + beforeBirthday);
+    }
+}
